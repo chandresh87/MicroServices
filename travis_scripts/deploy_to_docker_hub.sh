65491f04d48e33032cbb939dfe6e83830a44556e
Chandresh Mishra <chandresh87@gmail.com>
2019-10-18 21:34:49 +0100

    image tag

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,9 +1,9 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push com.microservicePlayground/eurekaservice:$BUILD_NAME
-docker push com.microservicePlayground/configservice:$BUILD_NAME
-docker push com.microservicePlayground/auth-server:$BUILD_NAME
-docker push com.microservicePlayground/organizationmicroservice:$BUILD_NAME
-docker push com.microservicePlayground/personmicroservice:$BUILD_NAME
-docker push com.microservicePlayground/turbinemicroservice:$BUILD_NAME
-docker push com.microservicePlayground/zuul-server:$BUILD_NAME
+docker push com.microservicePlayground/eurekaservice:1.0
+docker push com.microservicePlayground/configservice:1.0
+docker push com.microservicePlayground/auth-server:1.0
+docker push com.microservicePlayground/organizationmicroservice:1.0
+docker push com.microservicePlayground/personmicroservice:1.0
+docker push com.microservicePlayground/turbinemicroservice:1.0
+docker push com.microservicePlayground/zuul-server:1.0
