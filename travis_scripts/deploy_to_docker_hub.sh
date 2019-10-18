cd8296df3c97377b473af63a801ff2cbe8b26a5b
Chandresh Mishra <chandresh87@gmail.com>
2019-10-18 23:36:21 +0100

    image tag

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,4 +1,4 @@
 echo "Pushing service docker images to docker hub ...."
-docker login docker.io -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker tag microservice/configservice:1.0 configservice:$BUILD_NAME
-docker push configservice:$BUILD_NAME
+docker login  -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker tag microservice/configservice:1.0 chandreshmishra/configservice:$BUILD_NAME
+docker push chandreshmishra/configservice:$BUILD_NAME
