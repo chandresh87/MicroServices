105fe0bab5dd3f72e7811e914d5f8e29938a1f91
Chandresh Mishra <chandresh87@gmail.com>
2019-10-18 22:36:24 +0100

    image tag

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,9 +1,9 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push microservice/eurekaservice:1.0
-docker push microservice/configservice:1.0
-docker push microservice/auth-server:1.0
-docker push microservice/organizationmicroservice:1.0
-docker push microservice/personmicroservice:1.0
-docker push microservice/turbinemicroservice:1.0
-docker push microservice/zuul-server:1.0
+docker push chandreshmishra/microservice:microservice/eurekaservice:1.0
+docker push chandreshmishra/microservice:microservice/configservice:1.0
+docker push chandreshmishra/microservice:microservice/auth-server:1.0
+docker push chandreshmishra/microservice:microservice/organizationmicroservice:1.0
+docker push chandreshmishra/microservice:microservice/personmicroservice:1.0
+docker push chandreshmishra/microservice:microservice/turbinemicroservice:1.0
+docker push chandreshmishra/microservice:microservice/zuul-server:1.0
