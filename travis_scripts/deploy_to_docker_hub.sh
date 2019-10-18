cdb1fdb43c7ba13a0b7f5be084c2ae38c7352cbd
Chandresh Mishra <chandresh87@gmail.com>
2019-10-18 21:18:03 +0100

    image name

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,9 +1,9 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push chandreshmishra/eurekaservice:$BUILD_NAME
-docker push chandreshmishra/configservice:$BUILD_NAME
-docker push chandreshmishra/auth-server:$BUILD_NAME
-docker push chandreshmishra/organizationmicroservice:$BUILD_NAME
-docker push chandreshmishra/personmicroservice:$BUILD_NAME
-docker push chandreshmishra/turbinemicroservice:$BUILD_NAME
-docker push chandreshmishra/zuul-server:$BUILD_NAME
+docker push com.microservicePlayground/eurekaservice:$BUILD_NAME
+docker push com.microservicePlayground/configservice:$BUILD_NAME
+docker push com.microservicePlayground/auth-server:$BUILD_NAME
+docker push com.microservicePlayground/organizationmicroservice:$BUILD_NAME
+docker push com.microservicePlayground/personmicroservice:$BUILD_NAME
+docker push com.microservicePlayground/turbinemicroservice:$BUILD_NAME
+docker push com.microservicePlayground/zuul-server:$BUILD_NAME
