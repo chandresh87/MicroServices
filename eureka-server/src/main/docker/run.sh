840fc9210a4753acae1574dacd4c0989c15e08e0
Chandresh Mishra <chandresh87@gmail.com>
2019-10-20 13:00:08 +0100

    docker

--- eureka-server/src/main/docker/run.sh
+++ eureka-server/src/main/docker/run.sh
@@ -3,4 +3,4 @@
 echo "********************************************************"
 echo "Starting the Eureka Server"
 echo "********************************************************"
-java  -Dspring.security.user.name=$EUREKA_USERNAME -Dspring.security.user.password=$EUREKA_PASSWORD -jar /usr/local/eurekaserver/eureka-server-0.0.1-SNAPSHOT.jar
+java -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI -Dspring.security.user.name=$EUREKA_USERNAME -Dspring.security.user.password=$EUREKA_PASSWORD -jar /usr/local/eurekaserver/eureka-server-0.0.1-SNAPSHOT.jar
